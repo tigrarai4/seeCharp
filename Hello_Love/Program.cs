9ad86f434ba93df120c9cfd59910d077b046d39f
Tatiana <raitman77@yandex.ru>
2023-10-12 13:25:16 +0300

    Магия

--- Hello_Love/Program.cs
+++ Hello_Love/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите имя пользователя");
 string username = Console.ReadLine();
 
-if(username == "Маша")
+if(username.ToLower() == "маша")
 {
     Console.WriteLine("Привет, красавица!");
 }
